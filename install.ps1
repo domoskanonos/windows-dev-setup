199f06660e6a332c6ccc60ca5f206ee2d62c0a04
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-12 16:07:59 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,7 +9,7 @@ If(test-path $global:WINDOWS_SETUP_PATH )
 {
 Remove-Item $global:WINDOWS_SETUP_PATH -Recurse -Force
 }
-Invoke-WebRequest https://git.system.local/rest/api/latest/projects/~U150239/repos/windows-setup/archive?format=zip -OutFile $global:INSTALLATION_PATH/windows-setup.zip
+Invoke-WebRequest https://github.com/koshisinthehouse/windows-dev-setup/archive/refs/heads/main.zip -OutFile $global:INSTALLATION_PATH/windows-setup.zip
 Get-ChildItem $global:INSTALLATION_PATH -Filter windows-setup.zip | Expand-Archive -DestinationPath $global:WINDOWS_SETUP_PATH -Force
 
 #powershell.exe "$global:WINDOWS_SETUP_PATH/setup.ps1 -USER $USER -USER_EMAIL $USER_EMAIL"
