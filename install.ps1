a1a4ba082254940b94f1ab12d580e63dbcc31eb2
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-12 16:13:43 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -7,4 +7,4 @@ If(-Not (test-path $global:INSTALLATION_PATH) )
 Invoke-WebRequest https://github.com/koshisinthehouse/windows-dev-setup/archive/refs/heads/main.zip -OutFile $global:INSTALLATION_PATH/windows-setup.zip
 Get-ChildItem $global:INSTALLATION_PATH -Filter windows-setup.zip | Expand-Archive -DestinationPath $global:INSTALLATION_PATH -Force
 
-#powershell.exe "$global:WINDOWS_SETUP_PATH/setup.ps1 -USER $USER -USER_EMAIL $USER_EMAIL"
+#powershell.exe "$global:INSTALLATION_PATH/windows-dev-setup-main/setup.ps1 -USER $USER -USER_EMAIL $USER_EMAIL"
