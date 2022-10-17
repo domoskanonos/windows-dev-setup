1d7abd1a83bfd4466c18d853cab8f7187aa16104
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-17 11:14:06 +0200

    Update gitClone.ps1

--- gitClone.ps1
+++ gitClone.ps1
@@ -1,5 +1,8 @@
-param ([string] $REPOSITORY, $DEST)
+param ([string] $REPOSITORY, $DEST, $CHANGE_REPO)
 If(-Not (test-path $DEST) ) {
     Write-Output "clone repo: $REPOSITORY, dest: $DEST"
     git clone $REPOSITORY $DEST
+    If($CHANGE_REPO) {
+        git remote set-url origin $CHANGE_REPO
+    }    
 }
