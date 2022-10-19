0acba15d76ef0f41c74501ec044c4869867f5c5a
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-19 20:38:17 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,10 +3,6 @@ $global:INSTALLATION_PATH = $INSTALLATION_PATH
 $global:WINDOWS_SETUP_PATH = $WINDOWS_SETUP_PATH
 $global:DOWNLOAD_PATH = (Join-Path $global:INSTALLATION_PATH \downloads)
 
-
-exit
-
-
 If(-Not (test-path $global:DOWNLOAD_PATH) )
 {
     New-Item -ItemType Directory -Force -Path $DOWNLOAD_PATH
