bb7d56c9d60cb39f74e41e1e0e97ea2eb8080d50
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-19 20:41:02 +0200

    Update gitClone.ps1

--- gitClone.ps1
+++ gitClone.ps1
@@ -1,5 +1,4 @@
 param ([string] $REPOSITORY, $DEST, $CHANGE_REPO)
-echo "CLONI $DEST"
 If(-Not (test-path $DEST) ) {
     Write-Output "clone repo: $REPOSITORY, dest: $DEST"
     git clone $REPOSITORY $DEST
