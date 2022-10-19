5613ab6ea4ad55d1dc61a11c3a9962152d782b70
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-19 16:12:49 +0200

    Update gitClone.ps1

--- gitClone.ps1
+++ gitClone.ps1
@@ -3,6 +3,9 @@ If(-Not (test-path $DEST) ) {
     Write-Output "clone repo: $REPOSITORY, dest: $DEST"
     git clone $REPOSITORY $DEST
     If($CHANGE_REPO) {
+    	Set-Variable -Name "currentDirectory" -Value ([System.Environment]::CurrentDirectory)
+        cd $DEST
         git remote set-url origin $CHANGE_REPO
+        cd $currentDirectory
     }    
 }
