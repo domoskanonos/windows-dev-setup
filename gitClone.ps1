59d1884b7df1f37e49eb011137dd478259220cff
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-19 20:10:48 +0200

    Update gitClone.ps1

--- gitClone.ps1
+++ gitClone.ps1
@@ -4,10 +4,12 @@ If(-Not (test-path $DEST) ) {
     git clone $REPOSITORY $DEST
     If($CHANGE_REPO) {
     	
-       
-        Invoke-Expression "$DESTgit remote set-url origin $CHANGE_REPO"
+       $repoPath = (Join-Path $DEST \git)
+        echo "current: $repoPath remote set-url origin $CHANGE_REPO"
         
-        echo "current: $DESTgit remote set-url origin $CHANGE_REPO"
+        Invoke-Expression "$repoPath remote set-url origin $CHANGE_REPO"
+        
+       
         
         
   }    
