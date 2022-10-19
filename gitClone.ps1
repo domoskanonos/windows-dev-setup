84b8d46d757b2262314f2164b6e78cdf0b74a5e0
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-10-19 20:08:02 +0200

    Update gitClone.ps1

--- gitClone.ps1
+++ gitClone.ps1
@@ -3,14 +3,12 @@ If(-Not (test-path $DEST) ) {
     Write-Output "clone repo: $REPOSITORY, dest: $DEST"
     git clone $REPOSITORY $DEST
     If($CHANGE_REPO) {
-    	Set-Variable -Name "currentDirectory" -Value ($PSScriptRoot)
-        echo "change git repo, current directory: $currentDirectory"
-        cd $DEST
-        echo "destination: $CHANGE_REPO"
-        git remote set-url origin $CHANGE_REPO
-        echo "current: ([System.Environment]::CurrentDirectory)"
+    	
+       
+        Invoke-Expression "$DESTgit remote set-url origin $CHANGE_REPO"
         
-        cd $currentDirectory
-          echo "current 2: ([System.Environment]::CurrentDirectory)"
+        echo "current: $DESTgit remote set-url origin $CHANGE_REPO"
+        
+        
   }    
 }
