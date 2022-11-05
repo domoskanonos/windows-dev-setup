e8878b367456436fd5f44a37169ef78492a26952
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-11-05 19:48:25 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -30,7 +30,7 @@ If(-Not (test-path $SSH_PATH) )
     New-Item -ItemType Directory -Force -Path $SSH_PATH
     Copy-Item "$global:WINDOWS_SETUP_PATH\config" -Destination "$SSH_PATH" -Recurse -force
 }
-Invoke-Expression "$global:WINDOWS_SETUP_PATH\ssh.ps1 -FILENAME $SSH_PATH/koshisinthehouse -EMAIL koshisinthehouse@googlemail.com"
+Invoke-Expression "$global:WINDOWS_SETUP_PATH\ssh.ps1 -SSH_FILE_PATH $SSH_PATH/koshisinthehouse -EMAIL koshisinthehouse@googlemail.com"
 #ssh - end
 
 #putty - start
