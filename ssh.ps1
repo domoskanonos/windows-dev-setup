def9277e9b3cdcfcc4aa80343eb81ddefaa55aff
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2022-11-05 19:43:28 +0100

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,12 +1,5 @@
-param ([string] $FILENAME, $EMAIL)
+param ([string] $SSH_FILE_PATH, $EMAIL)
 #ssh key pair - start
-$SSH_PATH = "$home\.ssh";
-If(-Not (test-path $SSH_PATH) )
-{
-    New-Item -ItemType Directory -Force -Path $SSH_PATH
-    Copy-Item "$global:WINDOWS_SETUP_PATH\config" -Destination "$SSH_PATH" -Recurse -force
-}
-$SSH_FILE_PATH = "$SSH_PATH\$FILENAME";
 If(-Not (test-path $SSH_FILE_PATH) )
 {
     ssh-keygen -t ed25519 -f $SSH_FILE_PATH -N '""' -C "$EMAIL"
