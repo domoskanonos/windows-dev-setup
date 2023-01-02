53a0505b1bd67c5f4eba3dd2021903861d0ea8fd
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2023-01-02 11:46:19 +0100

    Update env.ps1

--- env.ps1
+++ env.ps1
@@ -2,4 +2,6 @@ param ([string] $KEY, $VALUE, $PATH_SUFFIX)
 $PATH =(Join-Path $VALUE $PATH_SUFFIX)
 Write-Output "env.ps1: key: $KEY, value: $VALUE, pathSuffix: $PATH_SUFFIX, path: $PATH"
 [System.Environment]::SetEnvironmentVariable($KEY, $VALUE, "Machine")
-Invoke-Expression "$global:WINDOWS_SETUP_PATH\envPath.ps1 -PATH '$PATH'"
+[System.Environment]::SetEnvironmentVariable($KEY, $VALUE, "User")
+Invoke-Expression "$global:WINDOWS_SETUP_PATH\envPath.ps1 -PATH '$PATH' -TYPE 'Machine'"
+Invoke-Expression "$global:WINDOWS_SETUP_PATH\envPath.ps1 -PATH '$PATH' -TYPE 'User'"
