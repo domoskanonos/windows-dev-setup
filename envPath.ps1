0cfbd8fd0d4d9553666be7c272657df6cd0ad501
koshisinthehouse <113584179+koshisinthehouse@users.noreply.github.com>
2023-01-02 10:44:08 +0100

    Update envPath.ps1

--- envPath.ps1
+++ envPath.ps1
@@ -2,9 +2,9 @@ param ([string] $PATH)
 $envPath = [Environment]::GetEnvironmentVariable('Path', 'User');
 if(-Not $envPath.Contains("$PATH")){
     Write-Output "add to env path, value: $PATH"
-    [System.Environment]::SetEnvironmentVariable("Path", $envPath + ";$PATH", "User")
+    [System.Environment]::SetEnvironmentVariable("Path", $envPath + ";$PATH")
 }
 else {
     Write-Output "path in env path var already exist, ignore path value: $PATH"
 }
-$Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "User")
+$Env:Path = [System.Environment]::GetEnvironmentVariable("Path")
