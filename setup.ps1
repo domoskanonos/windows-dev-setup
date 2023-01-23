f08f3d62e16cdd0c30357f1e4b999c6de72bcbe9
Dominik Bruhn <49183249+domoskanonos@users.noreply.github.com>
2023-01-23 12:10:01 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -62,9 +62,9 @@ git config --global http.sslVerify true
 #git config --global --replace-all user.email "$USER_EMAIL"
 #git - end
 #node - start
-$NODE_PATH ="$global:EXTRACT_PATH\node-v16.18.0-win-x64"
+$NODE_PATH ="$global:EXTRACT_PATH\node"
 $NPM_PATH = "$NODE_PATH\node_modules\npm\bin"
-Invoke-Expression "$global:WINDOWS_SETUP_PATH\download.ps1 -DOWNLOAD_PATH '$global:DOWNLOAD_PATH' -ZIP_FILE_NAME 'node-v16.18.0-win-x64.zip' -ZIP_FILE_URL 'https://nodejs.org/dist/v16.18.0/node-v16.18.0-win-x64.zip' -EXTRACT_PATH '$global:EXTRACT_PATH' -CHECK_PATH '$NODE_PATH'"
+Invoke-Expression "$global:WINDOWS_SETUP_PATH\download.ps1 -DOWNLOAD_PATH '$global:DOWNLOAD_PATH' -ZIP_FILE_NAME 'node-v19.4.0-win-x64.zip' -ZIP_FILE_URL 'https://nodejs.org/dist/latest-v19.x/node-v19.4.0-win-x64.zip' -EXTRACT_PATH '$global:EXTRACT_PATH' -CHECK_PATH '$NODE_PATH'"
 Invoke-Expression "$global:WINDOWS_SETUP_PATH\env.ps1 -KEY 'NODE_HOME' -VALUE '$NODE_PATH'"
 Invoke-Expression "$global:WINDOWS_SETUP_PATH\envPath.ps1 -PATH '$NPM_PATH' -TYPE 'User'"
 Invoke-Expression "$global:WINDOWS_SETUP_PATH\envPath.ps1 -PATH '$NPM_PATH' -TYPE 'Machine'"
